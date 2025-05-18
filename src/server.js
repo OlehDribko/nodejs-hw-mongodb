9e755ff5c981abe65324ba761a2d3272b4350038
Ваше ім'я < email>
2025-05-18 16:04:33 +0300

    clg

--- src/server.js
+++ src/server.js
@@ -24,7 +24,6 @@ export const setupServer = () => {
 
   app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
-    console.log(contactId);
     const contact = await getContactById(contactId);
     if (!contact) {
       return res.status(404).json({
