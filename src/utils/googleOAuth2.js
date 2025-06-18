09e3b2b60ad8c00e9543078d5ccc171bcc37a1ed
Ваше ім'я < email>
2025-06-19 00:20:03 +0300

    remove secret file from git history

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -5,7 +5,7 @@ import { readFile } from 'fs/promises';
 import { getEnvWar } from './getEnv.js';
 import createHttpError from 'http-errors';
 
-const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
+// const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
 
 const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
